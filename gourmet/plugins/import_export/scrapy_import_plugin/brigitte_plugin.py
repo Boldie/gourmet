4803a23c0dcb0fa5968ecf90c3daf2e848054fad
Sven Steckmann <spm_gitcommit@steckmann.de>
2015-08-16 13:21:50 +0200

    Write a modern and better to test scrapy import plugin which uses scrapy selectors.
    The scrapy selectors are easy to use while parsing websites. The essen und
    trinken page was implemented as a first test. Furthermore there is a test-case
    attached which enables us to create reference data from the webpage to ensure
    the plugins will stay working. They use a set of reference data which can
    be produced also by running the testcase itself. This makes it easy to
    maintain because only the outcome has to be checked.

---
+++ gourmet/plugins/import_export/scrapy_import_plugin/brigitte_plugin.py
@@ -0,0 +1,63 @@
+from gourmet.plugin import PluginPlugin
+import re
+
+class BrigittePlugin (PluginPlugin):
+    """ This is a plugin for the german Brigitte page to import recipes
+    easily.
+    """
+    target_pluggable = 'scrapyWeb_plugin'
+    internalName = 'brigitte'
+
+    def do_activate (self, pluggable):
+        pass
+    
+    def test_url (self, url, data):
+        if 'brigitte.de' in url: 
+            return 10
+        return -99
+
+    def get_parser (self):
+        class Parser:
+            def parse(self, selector):
+                recipe = dict()
+                recipe['source'] = "Brigitte"
+                
+                recipe['title'] = selector.css(".article-headline-recipe::text").extract_first()
+                
+                ingredientsWithGroups = []
+                ingredients = []
+                
+                ingBlocks = selector.css(".o-article__ingredient-block")
+                for block in ingBlocks:
+                    head = block.xpath(".//h4/text()").extract_first()
+                    if head and len(head) > 0:
+                        gDict = { 'name':head, 'ingredients':[] }
+                        ingredientsWithGroups.append( gDict )
+                        ingredients = gDict['ingredients']
+                    
+                    for ing in block.xpath(".//li"):
+                        amount = ing.xpath("@data-recipe-item-amount").extract_first()
+                        unit = ing.xpath("@data-recipe-item-unit-singular").extract_first()
+                        name = ing.xpath("@data-recipe-item-name-singular").extract_first()
+                        add = ing.xpath("@data-recipe-item-name-suffix").extract_first()
+                        if len(add) > 0:
+                            add = "(" + add + ")"
+                        
+                        ingredients.append(" ".join( filter(lambda k: len(k) > 0, [amount, unit, name, add] )) )
+                                    
+                recipe['ingredients'] = ingredients if len( ingredientsWithGroups ) == 0 else ingredientsWithGroups
+                recipe['servings'] = int(selector.css(".o-article_select")[0].xpath(".//option[@selected]/text()").extract_first())
+                
+                total = selector.xpath("//time[@itemprop='totalTime']/text()").extract_first()
+                m = re.search(u"([0-9]+)[\s\xa0\n]*(\S+)", total,re.MULTILINE | re.DOTALL)
+                if m:
+                    recipe['cooktime'] = " ".join( [m.group(1), m.group(2)])
+                
+                recipe['instructions'] = "\n\n".join( selector.css(".o-article__preparation-wrapper").xpath(".//span[@class='m-ordered-list-recipe__preparation-text']/text()").extract() )
+                recipe['imageUrl'] = selector.css(".m-single-image--aufmacher").xpath(".//img/@src").extract_first()
+
+                return recipe;
+
+        return Parser()
+
+
